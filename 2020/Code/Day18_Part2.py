b2577f7a35d43c61e5582e32a1d45c91250ded99
Philippe Merariu <lmerariu@gmail.com>
2020-12-18 16:34:31 -0500

    2020-day18 complete

---
+++ 2020/Code/Day18_Part2.py
@@ -0,0 +1,62 @@
+def calculate(eq):
+    count = 0
+    for symb in eq:
+
+        if symb.isdigit():
+            order.get(count)[0].append(int(symb))
+        elif symb in ['+', '*']:
+            order.get(count)[1].append(symb)
+        elif symb == '(':
+            nb.clear()
+            op.clear()
+            count += 1
+            order.update({count: [nb.copy(), op.copy()]})
+        elif symb == ')':
+            nb.clear()
+            op.clear()
+            count -= 1
+            order.get(count)[0].append(operation())
+            order.pop(count+1)
+    return operation()
+
+def operation():
+    data = order.get(max(order))
+    nbs = data[0]
+    ops = data[1]
+    offset = 0
+    for i in range(len(ops)):
+        if ops[i] == '+':
+            nbs.insert(i-offset, nbs.pop(i-offset) + nbs.pop(i-offset))
+            offset += 1
+    while ops.count('+') > 0:
+        ops.remove('+')
+    res = nbs[0]
+    for i in range(len(ops)):
+        if ops[i] == '+':
+            res += nbs[i+1]
+        else:
+            res *= nbs[i+1]
+    return res
+
+
+op = []
+nb = []
+order = {0: [nb.copy(), op.copy()]}
+total = 0
+
+file = open('input18.txt')
+line = file.readline().replace('\n', '')
+# line = '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 '
+# line = '2 * 3 + (4 * 5)'
+# total += calculate(line.replace(' ', ''))
+while line:
+    total += calculate(line.replace(' ', ''))
+    print("subtotal: " + str(total))
+    nb.clear()
+    op.clear()
+    order = {0: [nb.copy(), op.copy()]}
+    line = file.readline().replace('\n', '')
+file.close()
+
+print("Result: " + str(total))
+
