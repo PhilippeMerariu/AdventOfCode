ebfdfc9fd1f37aa42770cc707ba41aaa2c3e6f8e
PhilippeMerariu <lmerariu@gmail.com>
2021-12-05 17:14:29 -0300

    completed day5 part1

---
+++ 2021/Day5_Part2.py
@@ -0,0 +1,12 @@
+from typing import List
+
+file = open('input5.txt')
+line = file.readline().strip()
+
+while line:
+    line = file.readline()
+file.close()
+
+answer: int = 0
+
+print("ANSWER = {0}".format(answer))
