d50aa7a51c84133993f549fdf3038fce01acb8eb
PhilippeMerariu <lmerariu@gmail.com>
2021-12-06 09:49:02 -0300

    completed day6 part1

--- 2021/Day6.py
+++ 2021/Day6.py
@@ -1,14 +1,31 @@
 from typing import List
 
+NEW_FISH_DAYS: int = 8
+FISH_DAYS: int = 6
+NB_OF_DAYS: int = 80
+
 file = open('input6.txt')
 line = file.readline().strip()
 
+fish: List[int] = [int(x) for x in line.split(",")]
 
-while line:
-    line = file.readline()
 file.close()
 
-answer: int = 0
+baby_fish: List[int] = []
+
+days: int = 0
+while days < NB_OF_DAYS:
+    for i in range(len(fish)):
+        if fish[i] == 0:
+            baby_fish.append(NEW_FISH_DAYS)
+            fish[i] = FISH_DAYS
+        else:
+            fish[i] -= 1
+    fish = fish + baby_fish.copy()
+    baby_fish.clear()
+    days += 1
+
+answer: int = len(fish)
 
 
 print("ANSWER = {0}".format(answer))
