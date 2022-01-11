cc573adde2607f8610d45ef1136e3dd16679f880
PhilippeMerariu <lmerariu@gmail.com>
2022-01-11 10:15:32 -0500

    almost complete part 1

--- 2021/Day11.py
+++ 2021/Day11.py
@@ -1,11 +1,12 @@
-from typing import List
+from typing import List, Tuple
 
-STEPS: int = 100
+STEPS: int = 2
 
 file = open('input11_test.txt')
 line = file.readline().strip()
 
 octopus_grid: List[List[int]] = []
+energy_bursts: List[Tuple[int, int]] = []
 flashing_lights: int = 0
 
 count: int = 0
@@ -17,6 +18,78 @@ while line:
     line = file.readline().strip()
 file.close()
 
-answer: int = 0
 
-print("ANSWER = {0}".format(answer))
+def increase_energy(row: int, col: int, adjacent: bool = False) -> None:
+    # skip if row or col is beyond grid bounds
+    if row <= -1 or row >= len(octopus_grid) or col <= -1 or col >= len(octopus_grid):
+        return
+    # skip adding energy to octopus that already flashed
+    if adjacent and octopus_grid[row][col] == 0:
+        return
+    if octopus_grid[row][col] < 9:
+        octopus_grid[row][col] += 1
+    else:
+        octopus_grid[row][col] = 0
+        energy_bursts.append((row, col))
+
+
+def increase_adjacent_energy(center: Tuple[int, int]) -> None:
+    r: int = center[0]
+    c: int = center[1]
+    # top left
+    try:
+        increase_energy(r - 1, c - 1, True)
+    except IndexError:
+        pass  # nothing to do
+    # top center
+    try:
+        increase_energy(r - 1, c, True)
+    except IndexError:
+        pass  # nothing to do
+    # top right
+    try:
+        increase_energy(r - 1, c + 1, True)
+    except IndexError:
+        pass  # nothing to do
+    # center left
+    try:
+        increase_energy(r, c - 1, True)
+    except IndexError:
+        pass  # nothing to do
+    # center right
+    try:
+        increase_energy(r, c + 1, True)
+    except IndexError:
+        pass  # nothing to do
+    # bottom left
+    try:
+        increase_energy(r + 1, c - 1, True)
+    except IndexError:
+        pass  # nothing to do
+    # bottom center
+    try:
+        increase_energy(r + 1, c, True)
+    except IndexError:
+        pass  # nothing to do
+    # bottom right
+    try:
+        increase_energy(r + 1, c + 1, True)
+    except IndexError:
+        pass  # nothing to do
+
+
+for _ in range(STEPS):
+    energy_bursts.clear()
+    for row in range(len(octopus_grid)):
+        for col in range(len(octopus_grid[row])):
+            if octopus_grid[row][col] < 9:
+                octopus_grid[row][col] += 1
+            else:
+                flashing_lights += 1
+                octopus_grid[row][col] = 0
+                energy_bursts.append((row, col))
+
+    for burst in energy_bursts:
+        increase_adjacent_energy(burst)
+
+print("ANSWER = {0}".format(flashing_lights))
