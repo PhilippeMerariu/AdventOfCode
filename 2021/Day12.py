a936a23dc07b5cdd9ef3288d24cf7c7d19369217
PhilippeMerariu <lmerariu@gmail.com>
2022-01-13 14:44:04 -0500

    barely started part 1

--- 2021/Day12.py
+++ 2021/Day12.py
@@ -1,9 +1,31 @@
+from typing import List, Dict
+
+caves: List[str] = []
+start_caves: List[str] = []
+end_caves: List[str] = []
+
+connections: Dict[str, List[str]] = {}
+
 file = open('input12_test.txt')
 line = file.readline().strip()
 
 while line:
+    input_caves: List[str] = line.split("-")
+    if input_caves[0] not in connections:
+        connections.update({input_caves[0]: [input_caves[1]]})
+    else:
+        connections.update({input_caves[0]: connections.get(input_caves[0]).append(input_caves[1])})
+
+    if input_caves[1] not in connections:
+        connections.update({input_caves[1]: [input_caves[0]]})
+    else:
+        connections.update({input_caves[1]: connections.get(input_caves[1]).append(input_caves[0])})
     line = file.readline().strip()
 file.close()
+
+
+
+
 
 answer: int = 0
 print("ANSWER = {0}".format(answer))
