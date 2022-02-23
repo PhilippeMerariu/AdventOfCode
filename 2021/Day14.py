e19675865aeca6ddac4fc046d788f3c78579df55
Philippe Merariu <lmerariu@gmail.com>
2022-02-22 21:05:07 -0500

    started day 14

---
+++ 2021/Day14.py
@@ -0,0 +1,10 @@
+
+file = open('input14_test.txt')
+line = file.readline().strip()
+
+while line:
+    line = file.readline().strip()
+file.close()
+
+answer: int = 0
+print("ANSWER = {0}".format(answer))
