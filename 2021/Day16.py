b97883e19e9324f9c818212f4aec764e807091ce
Philippe Merariu <lmerariu@gmail.com>
2022-04-03 20:22:42 -0400

    completed part 1

--- 2021/Day16.py
+++ 2021/Day16.py
@@ -1,8 +1,10 @@
+from typing import List
+
 LITERAL_VALUE: int = 4
 SUBPACKET_LENGTH: int = 0
 NB_SUBPACKETS: int = 1
 
-file = open('input16_test2.txt')
+file = open('input16.txt')
 line = file.readline().strip()
 
 binary_str: str = str(bin(int(line, 16)))[2:]
@@ -10,6 +12,12 @@ while len(binary_str) % 4 != 0:
     binary_str = "0" + binary_str
 
 version_sum: int = 0
+subpacket_lengths: List[int] = []
+
+
+def subtract_length(val: int) -> None:
+    for i in range(len(subpacket_lengths)):
+        subpacket_lengths[i] -= val
 
 
 def read_version_and_type() -> int:
@@ -20,6 +28,7 @@ def read_version_and_type() -> int:
     binary_str = binary_str[6:]
 
     nb_bits: int = 6
+    subtract_length(nb_bits)
     version_sum += packet_version
 
     if packet_type_id == LITERAL_VALUE:
@@ -42,9 +51,10 @@ def read_literal() -> int:
             keep_reading = False
         literal_value_str += binary_str[1:5]
         binary_str = binary_str[5:]
-        literal_length += 5
+        literal_length += 4
     literal_value: int = int(literal_value_str, 2)
     print("LITERAL_VALUE: ", literal_value)
+    subtract_length(literal_length)
     return literal_length
 
 
@@ -52,14 +62,19 @@ def read_operator() -> None:
     global binary_str
     length_type: int = int(binary_str[0])
     binary_str = binary_str[1:]
+    subtract_length(1)
     if length_type == SUBPACKET_LENGTH:  # length of sub-packet
         subpacket_length: int = int(binary_str[0:15], 2)
+        subtract_length(15)
+        subpacket_lengths.append(subpacket_length)
         binary_str = binary_str[15:]
-        while subpacket_length > 0:
+        while subpacket_lengths[-1] > 0:
             subpacket_length -= read_version_and_type()
+        del subpacket_lengths[-1]
     elif length_type == NB_SUBPACKETS:  # number of sub-packets
         nb_subpackets: int = int(binary_str[0:11], 2)
         binary_str = binary_str[11:]
+        subtract_length(11)
         while nb_subpackets > 0:
             read_version_and_type()
             nb_subpackets -= 1
